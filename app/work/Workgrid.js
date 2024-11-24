10c4dc118a0dbbef5e42d6bd4fae11cd1ce81823
NC0312 <niketchawla983@gmail.com>
2024-11-24 11:56:21 +0530

    minor fix

--- app/work/Workgrid.js
+++ app/work/Workgrid.js
@@ -44,7 +44,7 @@ const WorkPageGrid = () => {
 
   return (
     <section className="bg-[#ECE4DA] min-h-screen py-16">
-      <div className="container mx-auto px-4 md:px-6">
+      <div className="container mx-auto px-2 sm:px-4 md:px-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {works.map((work, index) => (
             <div
--- app/work/page.js
+++ app/work/page.js
@@ -4,7 +4,7 @@ import WorkGrid from './Workgrid'
 function work() {
   return (
     <>
-    <section className="text-center py-16">
+    <section className="text-center py-16 overflow-x-hidden">
       <div className="container mx-auto">
         <h1 className="text-9xl pt-14 font-medium text-[#2e2e2e] font-serif">
           Our Clients
