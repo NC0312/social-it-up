0fc77f8f75d391d38cbbd59f8cb9676ce7ef8136
NC0312 <niketchawla983@gmail.com>
2024-11-24 11:58:13 +0530

    minor fix

--- app/work/page.js
+++ app/work/page.js
@@ -6,7 +6,7 @@ function work() {
     <>
     <section className="text-center py-16 overflow-x-hidden">
       <div className="container mx-auto">
-        <h1 className="text-9xl pt-14 font-medium text-[#2e2e2e] font-serif">
+        <h1 className="text-7xl md:text-9xl pt-14 font-medium text-[#2e2e2e] font-serif">
           Our Clients
         </h1>
         <p className="text-xl font-bold text-[#2e2e2e] mt-4 uppercase tracking-wider">
