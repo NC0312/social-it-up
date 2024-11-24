5eacb93e67d60d895e68f2c4fa2fd11033db992d
NC0312 <niketchawla983@gmail.com>
2024-11-24 11:49:42 +0530

    Work section done

--- app/work/page.js
+++ app/work/page.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import WorkGrid from './Workgrid'
 
 function work() {
   return (
-    <div>
-      Work Section
-    </div>
+    <>
+    <section className="text-center py-16">
+      <div className="container mx-auto">
+        <h1 className="text-9xl pt-14 font-medium text-[#2e2e2e] font-serif">
+          Our Clients
+        </h1>
+        <p className="text-xl font-bold text-[#2e2e2e] mt-4 uppercase tracking-wider">
+          And Their Story.
+        </p>
+      </div>
+    </section>
+    <WorkGrid/>
+    </>
   )
 }
 
