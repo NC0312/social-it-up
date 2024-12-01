e476d3dabb2ef3861fceac03e734ea36afb8e522
NC0312 <niketchawla983@gmail.com>
2024-12-01 18:54:26 +0530

    minor fix

--- app/inquire/page.js
+++ app/inquire/page.js
@@ -154,6 +154,7 @@ const Inquire = () => {
                   exit={{ opacity: 0, y: -10 }}
                   transition={{ duration: 0.5 }}
                   className="text-sm text-[#36302A] mt-2 bg-[#EFE7DD] px-4 py-2 rounded-lg border border-[#36302A]"
+                  style={{userSelect:"none"}}
                 >
                   Signed up successfully! 🎊  
                 </motion.div>
