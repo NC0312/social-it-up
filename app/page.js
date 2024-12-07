42fe7410177e64a18ac9d94e43b374b4e4064c01
NC0312 <niketchawla983@gmail.com>
2024-12-07 14:01:59 +0530

    minor change

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
       <h1 className="text-xl font-bold">
-        Building Social it up...
+        It's Time for the home page...
       </h1>
     </div>
   );
