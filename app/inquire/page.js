ca8be91955b387bd165c4ac3cda46165c69dc771
NC0312 <niketchawla983@gmail.com>
2024-12-07 11:20:12 +0530

    minor fix

--- app/inquire/page.js
+++ app/inquire/page.js
@@ -289,6 +289,23 @@ const Inquire = () => {
                 Sign up for news and updates
               </label>
             </div>
+            <AnimatePresence>
+              {message && (
+                <motion.div
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }}
+                  transition={{ duration: 0.5 }}
+                  className={`text-sm mt-2 px-4 py-2 rounded-lg border ${
+                    message === 'Signed up successfully!'
+                      ? 'text-green-700 bg-green-100 border-green-400'
+                      : 'text-red-700 bg-red-100 border-red-400'
+                  }`}
+                >
+                  {message}
+                </motion.div>
+              )}
+            </AnimatePresence>
 
             <p className="md:text-md pt-5 md:pt-2 text-[#36302A]">
               Phone <span className="text-sm text-[#86807A] ml-1">(required)</span>
