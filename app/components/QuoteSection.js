d3eafaf27604850b0afc48989e977e4b730626f1
NC0312 <niketchawla983@gmail.com>
2024-12-07 17:36:12 +0530

    minor fix

--- app/components/QuoteSection.js
+++ app/components/QuoteSection.js
@@ -5,10 +5,10 @@ const QuoteSection = () => {
   return (
     <div className="bg-[#36302A] text-[#F6F3EE] py-40 px-8 text-center">
       <p className="text-6xl font-serif font-medium mb-6">
-        "Branding is not just about being seen,
+        &quot;Branding is not just about being seen,
       </p>
       <p className="text-lg md:text-2xl font-serif mb-6">
-        it&apos;s about being remembered."
+        it&apos;s about being remembered.&quot;
         
       </p>
 
@@ -23,7 +23,7 @@ const QuoteSection = () => {
         }}
         viewport={{ once: true }}
       >
-        <div className="h-[2px] bg-[#998877] absolute bottom-[35px] left-[45px] w-full"></div>
+        <div className="h-[2px] bg-[#998877] absolute bottom-[35px] left-[45px] w-full sm-[20%]"></div>
       </motion.div>
       
     </div>
