e7da55b3d0a3d1121484daf355e972b9182ddbc2
Jatin-1111 <off.jatin1111@gmail.com>
2024-12-08 19:01:56 +0530

    FAQS SECTION

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Endorsements from "./components/Endorsements";
+import FAQs from "./components/faqs";
 import HomeExt1 from "./components/HomeExt1";
 import LegacyCarousel from "./components/LegacyCarousel";
 import OurServices from "./components/OurServices";
@@ -22,6 +23,7 @@ export default function Home() {
     <LegacyCarousel/>
     <Endorsements/>
     <OurServices/>
+    <FAQs/>
     </>
   );
 }
