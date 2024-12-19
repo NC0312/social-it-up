d2a951f6366b7b30171f79f2572aa5694402079e
NC0312 <niketchawla983@gmail.com>
2024-12-19 23:29:51 +0530

    minor fix

--- app/components/Header.js
+++ app/components/Header.js
@@ -47,17 +47,19 @@ function Header() {
 
   return (
     <div className="border-b border-[#575553] relative">
-      <nav
-        className={`flex items-center justify-between px-12 py-1 md:py-2 relative z-20 ${
-          isDevelopment || pathname === REVIEW_PANEL_ROUTE
-            ? "bg-green-600"
-            : pathname === BUG_PANEL_ROUTE
-            ? "bg-red-600"
-            : ""
-        }`}
-      >
+       <nav
+      className={`flex items-center justify-between px-12 py-1 md:py-2 relative z-20 ${
+        pathname === REVIEW_PANEL_ROUTE
+          ? "bg-green-600"
+          : pathname === BUG_PANEL_ROUTE
+          ? "bg-red-600"
+          : process.env.NEXT_PUBLIC_ENV === "development"
+          ? "bg-blue-600"
+          : ""
+      }`}
+    >
         {/* Logo */}
-        <div className="flex flex-row left-0 md:pl-5" style={{userSelect:"none"}}>
+        <div className="flex flex-row left-0 md:pl-5" style={{ userSelect: "none" }}>
           <Link href="/" passHref>
             <Image
               src="/logo.png"
@@ -69,22 +71,20 @@ function Header() {
           </Link>
 
           <p
-            className={`text-[10px] md:text-xs ${
-              isDevelopment || pathname === REVIEW_PANEL_ROUTE || pathname === BUG_PANEL_ROUTE
+            className={`text-[10px] md:text-xs ${isDevelopment || pathname === REVIEW_PANEL_ROUTE || pathname === BUG_PANEL_ROUTE
                 ? "text-white"
                 : "text-[#575553]"
-            } ml-2 mt-20`}
+              } ml-2 mt-20`}
           >
             Version by{" "}
             <Link
               href="https://niketchawla.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
-              className={`${
-                isDevelopment || pathname === REVIEW_PANEL_ROUTE || pathname === BUG_PANEL_ROUTE
+              className={`${isDevelopment || pathname === REVIEW_PANEL_ROUTE || pathname === BUG_PANEL_ROUTE
                   ? "text-white"
                   : "text-[#575553]"
-              } underline`}
+                } underline`}
             >
               Niket Chawla
             </Link>
@@ -92,16 +92,15 @@ function Header() {
         </div>
 
         {/* Menu Items (Desktop View) */}
-        <div className="hidden md:flex pr-12" style={{userSelect:"none"}}>
+        <div className="hidden md:flex pr-12" style={{ userSelect: "none" }}>
           <ul className="relative flex space-x-12 items-center">
             {["Home", "About", "Work", "Services", "Inquire"].map((item) => (
               <li
                 key={item}
-                className={`relative cursor-pointer ${
-                  isDevelopment || pathname === REVIEW_PANEL_ROUTE || pathname === BUG_PANEL_ROUTE
+                className={`relative cursor-pointer ${isDevelopment || pathname === REVIEW_PANEL_ROUTE || pathname === BUG_PANEL_ROUTE
                     ? "text-white"
                     : "text-[#575553]"
-                } text-md font-medium`}
+                  } text-md font-medium`}
                 onMouseEnter={() => setHoveredItem(item)}
                 onMouseLeave={() => setHoveredItem(null)}
               >
@@ -117,15 +116,14 @@ function Header() {
                 {(active === item || hoveredItem === item) && (
                   <motion.div
                     layoutId={active === item ? "activeUnderline" : "hoverUnderline"}
-                    className={`absolute bottom-0 left-0 ${
-                      isDevelopment || pathname === REVIEW_PANEL_ROUTE || pathname === BUG_PANEL_ROUTE
+                    className={`absolute bottom-0 left-0 ${isDevelopment || pathname === REVIEW_PANEL_ROUTE || pathname === BUG_PANEL_ROUTE
                         ? "bg-white"
                         : "bg-[#575553]"
-                    }`}
-                    style={{ 
-                      height: "2px", 
+                      }`}
+                    style={{
+                      height: "2px",
                       width: "100%",
-                      opacity: hoveredItem === item && active !== item ? 0.6 : 1 
+                      opacity: hoveredItem === item && active !== item ? 0.6 : 1
                     }}
                     initial={{ width: "0%" }}
                     animate={{ width: "100%" }}
@@ -147,25 +145,34 @@ function Header() {
                 </Link>
               </li>
             )}
+            {isDevelopment && (
+              <li className="hidden lg:block text-center ml-4">
+                <Link
+                  href="/bug-panel69"
+                  className="px-4 py-1 bg-[#ff8c00] text-white rounded-md hover:bg-[#ff9d33] transition text-sm lg:text-md"
+                >
+                  Bugs & Issues
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
 
         {/* Rest of the component remains the same */}
         {/* Development Mode Text */}
         {isDevelopment && (
-          <div className="absolute left-1/4 transform -translate-x-1/2 ml-36 md:ml-0 text-white text-xs md:text-lg font-light md:font-semibold" style={{userSelect:"none"}}>
-            Hi,Admin!👋 
+          <div className="absolute left-1/4 transform -translate-x-1/2 ml-36 md:ml-0 text-white text-xs md:text-lg font-light md:font-semibold" style={{ userSelect: "none" }}>
+            Hi,Admin!👋
           </div>
         )}
 
         {/* Mobile view components remain the same */}
-        <div className="md:hidden pr-5 relative z-50" style={{userSelect:"none"}}>
+        <div className="md:hidden pr-5 relative z-50" style={{ userSelect: "none" }}>
           <FaBars
-            className={`${
-              isDevelopment || pathname === REVIEW_PANEL_ROUTE || pathname === BUG_PANEL_ROUTE
+            className={`${isDevelopment || pathname === REVIEW_PANEL_ROUTE || pathname === BUG_PANEL_ROUTE
                 ? "text-white"
                 : "text-[#575553]"
-            } text-2xl cursor-pointer`}
+              } text-2xl cursor-pointer`}
             onClick={handleMenuToggle}
           />
         </div>
@@ -180,7 +187,7 @@ function Header() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             onClick={handleMenuToggle}
-            style={{userSelect:"none"}}
+            style={{ userSelect: "none" }}
           />
         )}
       </AnimatePresence>
@@ -193,16 +200,16 @@ function Header() {
             animate={{ x: 0 }}
             exit={{ x: "-100%" }}
             transition={{ type: "spring", stiffness: 300, damping: 30 }}
-            style={{userSelect:"none"}}
+            style={{ userSelect: "none" }}
           >
-            <div className="absolute top-4 right-4" style={{userSelect:"none"}}>
+            <div className="absolute top-4 right-4" style={{ userSelect: "none" }}>
               <FaTimes
                 className="text-[#575553] text-2xl cursor-pointer"
                 onClick={handleMenuToggle}
               />
             </div>
 
-            <div className="p-5" style={{userSelect:"none"}}>
+            <div className="p-5" style={{ userSelect: "none" }}>
               <div>
                 <Image
                   src="/logo.png"
@@ -213,13 +220,12 @@ function Header() {
               </div>
             </div>
 
-            <ul className="flex flex-col items-center space-y-6 pt-10" style={{userSelect:"none"}}>
+            <ul className="flex flex-col items-center space-y-6 pt-10" style={{ userSelect: "none" }}>
               {["Home", "About", "Work", "Services", "Inquire"].map((item) => (
                 <li
                   key={item}
-                  className={`cursor-pointer text-[#575553] text-lg font-medium pb-0.25 ${
-                    active === item ? "border-b border-[#575553]" : ""
-                  }`}
+                  className={`cursor-pointer text-[#575553] text-lg font-medium pb-0.25 ${active === item ? "border-b border-[#575553]" : ""
+                    }`}
                 >
                   <Link
                     href={item === "Home" ? "/" : `/${item.toLowerCase()}`}
@@ -242,6 +248,17 @@ function Header() {
                     onClick={handleMenuToggle}
                   >
                     Admin Panel
+                  </Link>
+                </li>
+              )}
+              {isDevelopment && (
+                <li className="pt-4">
+                  <Link
+                    href="/bug-panel69"
+                    className="px-4 py-2 bg-[#ff8c00] text-white rounded-md hover:bg-[#ff9d33]  transition"
+                    onClick={handleMenuToggle}
+                  >
+                    Bugs & Issues
                   </Link>
                 </li>
               )}
