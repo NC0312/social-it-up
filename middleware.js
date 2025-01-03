e1565dd36dfccf0a4b23ff472623fc685ca5a102
NC0312 <niketchawla983@gmail.com>
2025-01-04 02:59:36 +0530

    protecting the dashboard route

--- middleware.js
+++ middleware.js
@@ -2,18 +2,18 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-  // Get environment variable
+
   const env = process.env.NEXT_PUBLIC_ENV
   
-  // Define protected routes
-  const protectedRoutes = ['/admin-panel69', '/review-panel69', '/bug-panel69']
+
+  const protectedRoutes = ['/admin-panel69', '/review-panel69', '/bug-panel69','/rating-dashboard69']
   
-  // Check if the current path is a protected route
+
   const isProtectedRoute = protectedRoutes.some(route => 
     request.nextUrl.pathname.startsWith(route)
   )
   
-  // If it's production and trying to access protected route, redirect to 404
+
   if (env === 'production' && isProtectedRoute) {
     return NextResponse.rewrite(new URL('/404', request.url))
   }
@@ -26,6 +26,7 @@ export const config = {
   matcher: [
     '/admin-panel69/:path*',
     '/review-panel69/:path*',
-    '/bug-panel69/:path*'
+    '/bug-panel69/:path*',
+    '/rating-dashboard69/:path*'
   ]
 }
